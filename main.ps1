dfe3c5718fc48f5dd3af5b24d3d4cd804e4bf371
osawa-koki <github@osawa.dev>
2021-11-17 20:44:46 +0900

    helpコマンド作った。

--- main.ps1
+++ main.ps1
@@ -25,4 +25,23 @@ switch ($game) {
     Write-Host ""
     Exit 0
   }
+  help {
+    Write-Host ""
+    Write-Host "`tPut the game name in the fst parameter."
+    Write-Host "`t  You can select the game from the list below."
+    Write-Host "`t  - add-nums -> Add 4 numbers quickly and correctly."
+    Write-Host "`t  - just10 -> Measure JUST 10 seconds."
+    Write-Host ""
+    Write-Host "`tex) .\main.ps1 add-nums"
+    Write-Host "`tex) .\main.ps1 just10"
+    Write-Host "`tex) .\main.ps1 help"
+    Write-Host "`tex) .\main.ps1 version"
+    Write-Host ""
+    Write-Host "`t  or you can use the following commands."
+    Write-Host "`t  - help -> Show this help."
+    Write-Host "`t  - version -> Show the version."
+    Write-Host "`tex) .\main.ps1 help"
+    Write-Host "`tex) .\main.ps1 version"
+    Write-Host ""
+  }
 }
