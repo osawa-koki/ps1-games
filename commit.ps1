0faff355ada3a3dea0f0223e55f322f24fc6ae6d
osawa-koki <github@osawa.dev>
2021-11-18 00:15:04 +0900

    例外処理。

--- commit.ps1
+++ commit.ps1
@@ -2,7 +2,11 @@
 $message = Read-Host "message -> "
 
 $date = $(Get-Date).AddYears(-1)
-git add .
-git commit -m "$message" --date="$date"
-git rebase HEAD~1 --committer-date-is-author-date
-git push -u origin
+try {
+  git add .
+  git commit -m "$message" --date="$date"
+  git rebase HEAD~1 --committer-date-is-author-date
+  git push -u origin
+} catch {
+  Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+}
