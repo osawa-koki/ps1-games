79a3750d4963a9dc1de51407007592d274969949
osawa-koki <github@osawa.dev>
2021-11-17 20:13:20 +0900

    設定イロイロ。

--- commit.ps1
+++ commit.ps1
@@ -4,4 +4,4 @@ param(
 $date = $(Get-Date).AddYears(-1)
 git add .
 git commit -m "$message" --date="$date"
-git rebase HEAD~0 --committer-date-is-author-date
+git rebase HEAD~1 --committer-date-is-author-date
