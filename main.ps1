8a7e41ac87903316d44c5acfc648a68935b983b8
osawa-koki <github@osawa.dev>
2021-11-17 20:37:12 +0900

    コンソールクリア処理を削除。

--- main.ps1
+++ main.ps1
@@ -4,7 +4,6 @@ param(
 )
 
 if ($null -eq $game) {
-  Clear-Host
   Write-Host ""
   Write-Host "`tPut the game name in the fst parameter."
   Write-Host "`t  You can select the game from the list below."
