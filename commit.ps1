ac30ee4d0384f89f81794978d2cb837ce83ec830
osawa-koki <github@osawa.dev>
2021-11-18 14:43:01 +0900

    小さな修正。

--- commit.ps1
+++ commit.ps1
@@ -6,7 +6,7 @@ try {
   git add .
   git commit --allow-empty -m "$message" --date="$date"
   git rebase HEAD~1 --committer-date-is-author-date
-  git push -u origin
+  #git push -u origin
 } catch {
   Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
 }
